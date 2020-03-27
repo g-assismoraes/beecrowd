742a2e76fa990505bace6c7d31d76dd2eaf40865
g-assismoraes <62756365+g-assismoraes@users.noreply.github.com>
2020-03-27 13:15:45 -0300

    Create 2786.py

---
+++ INICIANTE/2786.py
@@ -0,0 +1,6 @@
+largura = int(input())
+comprimento = int(input())
+tipo1 = largura * comprimento + ((largura-1)*(comprimento-1))
+tipo2 = (largura-1)*2 + (comprimento-1)*2
+print(tipo1)
+print(tipo2)
