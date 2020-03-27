d9a001f10d10fa0512b89b946c04eed6520a0d73
g-assismoraes <62756365+g-assismoraes@users.noreply.github.com>
2020-03-27 12:13:40 -0300

    Create 1004.c

---
+++ INICIANTE/1004.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int a, b, prod = 0;
+    scanf("%d%d", &a, &b);
+
+    prod = a * b;
+    printf("PROD = %d\n", prod);
+
+    return 0;
+}
