59addd4a789c10076d9d6526470c37d041dc7bbf
g-assismoraes <62756365+g-assismoraes@users.noreply.github.com>
2020-03-27 12:17:32 -0300

    Create 1007.c

---
+++ INICIANTE/1007.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int a, b, c, d, diferenca = 0;
+    scanf("%d%d%d%d", &a, &b, &c, &d);
+
+    diferenca = a*b - c*d;
+
+    printf("DIFERENCA = %d\n", diferenca);
+
+    return 0;
+}
