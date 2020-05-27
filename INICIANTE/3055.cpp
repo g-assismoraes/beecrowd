2480e40843f3fbe04a54523b18fd7a3e8da03c6f
Gabriel Assis <assisgabriel@id.uff.br>
2020-05-27 12:14:43 -0300

    Create 3055.cpp

---
+++ INICIANTE/3055.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int notaA, notaB, media;
+    cin >> notaA >> media;
+    notaB = 2 * media - notaA;
+    cout << notaB << endl;
+    return 0;
+}
