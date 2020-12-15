21fde35f92441c3791e7b48a4acf9d9cfac27620
Gabriel Assis <assisgabriel@id.uff.br>
2020-12-15 18:48:48 -0300

    Create 1190.c

---
+++ INICIANTE/1190.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void ler_matriz(int len, float mat[len][len]){
+    for(int lin = 0; lin < len; lin++)
+        for(int col = 0; col < len; col++)
+            scanf("%f", &mat[lin][col]);
+}
+
+float soma_superior_direita(int len, float mat[len][len]){
+    float soma = 0.0;
+    for(int lin = 0; lin < len; lin++)
+        for(int col = 0; col < len; col++)
+            if((col >(11 - lin)) && (col > lin))
+                soma += mat[lin][col];
+    return soma;
+}
+
+void mostra_resposta(char op, float som){
+    if (op == 'S')
+        printf("%.1f\n", som);
+    else if( op == 'M')
+        printf("%.1f\n", som/30);
+}
+
+int main(){
+    float matriz[12][12];
+    char operacao;
+    scanf("%c", &operacao);
+
+    ler_matriz(12, matriz);
+
+    mostra_resposta(operacao, soma_superior_direita(12, matriz));
+
+    return 0;
+}
