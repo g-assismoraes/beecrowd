ffab14da20dc18c4cd190f7cb61a36c504203dba
Gabriel Assis <assisgabriel@id.uff.br>
2021-12-17 13:40:06 -0300

    Create 2619.sql

---
+++ SQL/2619.sql
@@ -0,0 +1,5 @@
+SELECT products.name, providers.name, products.price
+FROM products JOIN categories ON
+    products.id_categories = categories.id JOIN providers ON
+        products.id_providers = providers.id
+WHERE categories.name = 'Super Luxury' AND price > 1000;
